cd139d760de769eaa827b07eee2acdb4ceb4b3ef
Qolt <ysemenov.mail@gmail.com>
2013-08-17 21:10:34 +0400

    Revert "Added OpenID application. Added MySQL settings."
    This reverts commit 85d9caacce7a2e9b9bce83c25934be64c1e6cfd5.

--- notes/settings.py
+++ notes/settings.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dev',                      # Or path to database file if using sqlite3.
-        'USER': 'django_user',                      # Not used with sqlite3.
-        'PASSWORD': 'azatroot',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -123,20 +123,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes.notes_application',
-    'django_openid_auth',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
-
-AUTHENTICATION_BACKENDS = (
-    'django_openid_auth.auth.OpenIDBackend',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-OPENID_CREATE_USERS = True
-OPENID_UPDATE_DETAILS_FROM_SREG = True
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
