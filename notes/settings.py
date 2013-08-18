9754d1b0029a7ad68333556d3328425f7b974177
Qolt <ysemenov.mail@gmail.com>
2013-08-18 16:48:28 +0400

    Added and configured django-social-auth application.

--- notes/settings.py
+++ notes/settings.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dev',                      # Or path to database file if using sqlite3.
+        'USER': 'django_user',                      # Not used with sqlite3.
+        'PASSWORD': 'azatroot',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -123,12 +123,29 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'notes.notes_application',
+    'social_auth'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
 
+AUTHENTICATION_BACKENDS = (
+    'social_auth.backends.google.GoogleOAuth2Backend',
+    )
+
+GOOGLE_OAUTH2_CLIENT_ID      = '627999873306.apps.googleusercontent.com'
+GOOGLE_OAUTH2_CLIENT_SECRET  = '-784UErpsIT-F0Wf4XId6nEV'
+
+LOGIN_URL          = '/login-form/'
+LOGIN_REDIRECT_URL = '/logged-in/'
+LOGIN_ERROR_URL    = '/login-error/'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+    'social_auth.context_processors.social_auth_by_name_backends',
+)
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
--- notes/urls.py
+++ notes/urls.py
@@ -7,6 +7,7 @@ from notes_application.views import *
 
 urlpatterns = patterns('',
     ('^$', show_start_page),
+    url(r'', include('social_auth.urls')),
     # Examples:
     # url(r'^$', 'notes.views.home', name='home'),
     # url(r'^notes/', include('notes.foo.urls')),
