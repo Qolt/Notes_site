79bfc7f256176a5ea4b2545ed3a522efb2d02cdb
Qolt <ysemenov.mail@gmail.com>
2013-09-13 13:52:23 +0400

    Added "active" class to notes list. Fixed unbound bug in the edit_note form. Fixed note_id value in the edit_note script.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,2 @@
 $(function() {
-    $("#notes_list_ul a").click(function(){
-        var url = $(this).attr("href");
-        $("div.span6").load(url + '/ajax');
-        history.pushState({id: 'NOTE_ID'}, '', url); 
-        return false;
-    });
-    $(".note_text_area").click(function(){
-        $("div.span6").load("{% url edit_note%}{{ note_id }}");
-        return false;
-
-})
+});
